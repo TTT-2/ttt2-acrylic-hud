826cec9c3dba12d7b8ea75b78d13f885db7ff27a
Tim Goll <tim@timgoll.de>
2020-04-10 20:09:54 +0200

    added overwrite elements

--- lua/terrortown/gamemode/shared/huds/acrylic/cl_popup.lua
+++ lua/terrortown/gamemode/shared/huds/acrylic/cl_popup.lua
@@ -1,3 +1,17 @@
+local blur_hud, blur_base
+
 HUD.popupPaint = function(s, w, h)
-	DrawHUDElementBg(0, 0, w, h, s.paintColor)
+	if not IsValid(s.ply) then return end
+
+	if not hudelements or not huds then return end
+
+	blur_hud = blur_hud or huds.GetStored("acrylic")
+	blur_base = blur_base or hudelements.GetStored("acrylic_element")
+
+	if not blur_hud or not blur_base then return end
+
+	if not blur_hud.basecolor then return end
+
+	blur_base:DrawBg(0, 0, w, h, {r = s.paintColor.r, g = s.paintColor.g, b = s.paintColor.b, a = blur_hud.basecolor.a})
+	blur_base:DrawLines(0, 0, w, h)
 end
--- lua/terrortown/gamemode/shared/huds/acrylic/cl_voice.lua
+++ lua/terrortown/gamemode/shared/huds/acrylic/cl_voice.lua
@@ -1,17 +1,17 @@
-local blur_hud
+local blur_hud, blur_base
 
 HUD.voicePaint = function(s, w, h)
 	if not IsValid(s.ply) then return end
 
-	if not hudelements then return end
+	if not hudelements or not huds then return end
 
-	blur_hud = blur_hud or hudelements.GetStored("acrylic_element")
+	blur_hud = blur_hud or huds.GetStored("acrylic")
+	blur_base = blur_base or hudelements.GetStored("acrylic_element")
 
-	if not blur_hud then return end
+	if not blur_hud or not blur_base then return end
 
-	-- TODO basecolor is never defined?
 	if not blur_hud.basecolor then return end
 
-	blur_hud:DrawBg(0, 0, w, h, {r = s.Color.r, g = s.Color.g, b = s.Color.b, a = blur_hud.basecolor.a})
-	blur_hud:DrawLines(0, 0, w, h)
+	blur_base:DrawBg(0, 0, w, h, {r = s.Color.r, g = s.Color.g, b = s.Color.b, a = blur_hud.basecolor.a})
+	blur_base:DrawLines(0, 0, w, h)
 end
